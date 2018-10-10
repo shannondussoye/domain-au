e4689b6bf6709311062de80cc62e9de9e92f77c4
Shannon Dussoye <shannonduss@live.com>
2018-10-10 23:28:33 +1100

    async fixes

--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -1,6 +1,5 @@
 import unittest
 import pandas as pd
-import domain.simpleget as get
 
 # read csv
 class TestAdvancedGet(unittest.TestCase):
@@ -9,12 +8,6 @@ class TestAdvancedGet(unittest.TestCase):
         self.assertIsNotNone(post_c)
         self.assertEqual(post_c['postcode'][0],800)
         self.assertEqual(post_c['postcode'][155], 2147)
-
-# test simple get
-# class TestSimpleGet(unittest.TestCase):
-#     def test_simple_get(self):
-#         self.assertIsNotNone(
-#             get.simple_get('https://www.domain.com.au/rent/?excludedeposittaken=1&postcode=2000&page=1'))
 
 # TODO: Write test for reading json data
 # TODO: Write test for cleaning data
