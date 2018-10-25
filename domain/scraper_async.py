211cabde6c3fc28b520fddbb700f83f556c379db
Shannon Dussoye <shannonduss@live.com>
2018-10-26 00:52:57 +1100

    async fixed

--- domain/scraper_async.py
+++ domain/scraper_async.py
@@ -1,21 +1,36 @@
+import pandas as pd
 from aiohttp import ClientSession, TCPConnector
 from pypeln import asyncio_task as aio
-import pandas as pd
 from bs4 import BeautifulSoup
 import json
-import re
+import aiofiles
 
-list_data = []
-
-# read Postcode Fileç
+# Read Post Code File
 post_c = pd.read_csv('../data/Post Codes.csv', names=['postcode'])
+
+# Convert Post Code to generator
 post_i = iter(post_c['postcode'].tolist())
+
+# Create Url generator using Post Code
 urls = ("https://www.domain.com.au/rent/?excludedeposittaken=1&postcode={}&page=1".format(i) for i in post_i)
 
+limit = 100
+list_data = []
+multi_pages = []
 
-async def html_parser(html):
+
+
+async def fetch(url, session):
+    print("Scraping: " + url)
+    async with session.get(url) as response:
+        raw_data = await response.read()
+        if response.status == 200:
+            await parse(url, raw_data)
+
+
+async def parse(url, html):
     if str(html) != "None":
-        print("Parsing Html!")
+        print("Parsing Html!"+url)
         soup = BeautifulSoup(html, "html.parser")
         js = soup.find_all('script')
         for resultset in js:
@@ -25,50 +40,41 @@ async def html_parser(html):
                     "<script>window.renderizrData = (window.renderizrData || {}); window.renderizrData[\"page\"] = ",
                     " "). \
                     replace(";</script>", "").encode("utf-8").decode('utf-8', 'ignore').strip()
-                list_data.append(js_data)
+
+                json_data = (json.dumps(js_data))
+                async with aiofiles.open('../data/data.json', 'w+') as file:
+                    await file.write(json_data)
+
                 if 'totalPages' in json.loads(js_data):
                     pages = ((json.loads(js_data)['totalPages']) + 1)
-                else:
-                    pages = 0
-            else:
-                continue
-        return pages
-    else:
-        return 0
+                    sub_pages_url = [n for n in range(2, pages)]
+                    n_url = url.replace("&page=1", "&page={}")
+                    nurls = list(n_url.format(i) for i in sub_pages_url)
+                    await append(nurls)
 
 
-async def fetch(url, session):
-    async with session.get(url) as response:
-        print("Request")
-        raw_html = await response.read()
-        page_i = await html_parser(raw_html)
-        if page_i > 2:
-            for url_i in range(2, page_i):
-                n_p_url = str(re.sub(r'page=\d{1,}', 'page=' + str(url_i), url))
-                async with session.get(n_p_url) as n_response:
-                    print("Sub Request")
-                    raw_html2 = await n_response.read()
-                    await  html_parser(raw_html2)
+async def append(l):
+    multi_pages.append(l)
 
 
-def write():
-    print('Finished Scraping. Writing File:')
-    json_data = json.dumps(list_data)
-    file = open('../data/data_async.json', 'w')
-    file.write(json_data)
 
+aio.each(
+    fetch,
+    urls,
+    workers=limit,
+    on_start=lambda: ClientSession(connector=TCPConnector(limit=None, verify_ssl=False)),
+    on_done=lambda _status, session: session.close(),
+    run=True,
+)
 
-def main():
-    aio.each(
-        fetch,
-        urls,
-        workers=1000,
-        on_start=lambda: ClientSession(connector=TCPConnector(verify_ssl=False)),
-        on_done=lambda _status, session: session.close(),
-        run=True,
-    )
-    write()
+flat_multi_pages = [item for sublist in multi_pages for item in sublist]
+multi_p_url = iter(flat_multi_pages)
 
-
-if __name__ == "__main__":
-    main()
+aio.each(
+    fetch,
+    multi_p_url,
+    workers=limit,
+    on_start=lambda: ClientSession(connector=TCPConnector(limit=None, verify_ssl=False)),
+    on_done=lambda _status, session: session.close(),
+    run=True,
+)
