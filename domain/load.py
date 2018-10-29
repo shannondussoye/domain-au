48d46e30cc51c1fcc1ef72fb2cd5e800990a3583
Shannon Dussoye <shannonduss@live.com>
2018-10-29 22:54:23 +1100

    updated test

--- domain/load.py
+++ domain/load.py
@@ -1,3 +1,4 @@
 # TODO: Read data from Json File
 # TODO: Clean data
 # TODO: Load data into Elastic Seach or Mongo DB?
+
--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -1,15 +1,15 @@
 import unittest
 import pandas as pd
+import os
 
 # test to read post code
 class TestPostCodeFile(unittest.TestCase):
    def test_read_post_code(self):
-       post_c = pd.read_csv('../data/Post Codes.csv', names=['postcode'])
+       path = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'data','Post Codes.csv'))
+       post_c = pd.read_csv(path, names=['postcode'])
        self.assertIsNotNone(post_c)
        self.assertEqual(post_c['postcode'][0], 800)
        self.assertEqual(post_c['postcode'][155], 2147)
-
-
 
 
 # TODO: Write test for reading json data
