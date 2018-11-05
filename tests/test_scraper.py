5444bed28c1e3a371b019c96f3cd2e427c3900d7
Shannon Dussoye <shannonduss@live.com>
2018-11-05 20:15:40 +1100

    updated test

--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -1,6 +1,12 @@
 import unittest
 import pandas as pd
 import os
+import requests as re
+from bs4 import BeautifulSoup
+import json
+
+
+url = "https://www.domain.com.au/rent/?excludedeposittaken=1&postcode=2000&page=1"
 
 # test to read post code
 class TestPostCodeFile(unittest.TestCase):
@@ -10,6 +16,29 @@ class TestPostCodeFile(unittest.TestCase):
        self.assertIsNotNone(post_c)
        self.assertEqual(post_c['postcode'][0], 800)
        self.assertEqual(post_c['postcode'][155], 2147)
+
+# test request
+class TestRequest(unittest.TestCase):
+    def test_request(self):
+        response = re.get(url)
+        self.assertEqual(response.status_code,200)
+
+
+class TestParser(unittest.TestCase):
+    def test_request(self):
+        html = re.get(url)
+        soup = BeautifulSoup(html.content, "html.parser")
+        js = soup.find_all('script')
+        self.assertIsNotNone(js)
+        for resultset in js:
+            if str(resultset).startswith("<script>window.renderizrData = (window.renderizrData || {});"):
+                js_data = str(resultset)
+                js_data = js_data.replace(
+                    "<script>window.renderizrData = (window.renderizrData || {}); window.renderizrData[\"page\"] = ",
+                    " "). \
+                    replace(";</script>", "").encode("utf-8").decode('utf-8', 'ignore').strip()
+
+                self.assertIsNotNone(json.dumps(js_data))
 
 
 # TODO: Write test for reading json data
